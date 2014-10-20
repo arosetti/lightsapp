4d8e4ee2d9648b06e7dc1cad4afb5551ac02bb95
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2014-10-20 10:29:50 +0200

    adding LinearFilter class that implements Gaussian and Savitzky-Golay Smooting. updating graphs to use filtering. updating graphics settings.

--- app/src/main/java/com/lightsapp/lightsapp/GraphFragment.java
+++ app/src/main/java/com/lightsapp/lightsapp/GraphFragment.java
@@ -20,6 +20,7 @@ import com.jjoe64.graphview.GraphViewStyle;
 import com.jjoe64.graphview.LineGraphView;
 import com.lightsapp.camera.CameraController;
 import com.lightsapp.camera.Frame;
+import com.lightsapp.util.LinearFilter;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -60,9 +61,13 @@ public class GraphFragment extends Fragment {
         graphView_delay.setViewPort(2, 300);
         graphView_delay.setScrollable(true);
         graphView_delay.setScalable(true);
+        /*graphView_delay.setShowLegend(true);
+        graphView_delay.setLegendAlign(GraphView.LegendAlign.BOTTOM);
+        graphView_delay.setLegendWidth(200);*/
         graphView_delay.getGraphViewStyle().setGridStyle(GraphViewStyle.GridStyle.HORIZONTAL);
         graphView_delay.getGraphViewStyle().setGridColor(Color.rgb(30,30,30));
-        graphView_delay.getGraphViewStyle().setTextSize(getResources().getDimension(R.dimen.abc_action_bar_subtitle_text_size));
+        graphView_delay.getGraphViewStyle().setTextSize(10);
+        graphView_delay.getGraphViewStyle().setNumHorizontalLabels(0);
 
         LinearLayout layout = (LinearLayout) v.findViewById(R.id.graph1);
         layout.addView(graphView_delay);
@@ -72,9 +77,13 @@ public class GraphFragment extends Fragment {
         graphView_lum.setViewPort(2, 300);
         graphView_lum.setScrollable(true);
         graphView_lum.setScalable(true);
+        /*graphView_lum.setShowLegend(true);
+        graphView_lum.setLegendAlign(GraphView.LegendAlign.BOTTOM);
+        graphView_lum.setLegendWidth(200);*/
         graphView_lum.getGraphViewStyle().setGridStyle(GraphViewStyle.GridStyle.HORIZONTAL);
         graphView_lum.getGraphViewStyle().setGridColor(Color.rgb(30,30,30));
-        graphView_lum.getGraphViewStyle().setTextSize(getResources().getDimension(R.dimen.abc_action_bar_subtitle_text_size));
+        graphView_lum.getGraphViewStyle().setTextSize(10);
+        graphView_lum.getGraphViewStyle().setNumHorizontalLabels(0);
 
         layout = (LinearLayout) v.findViewById(R.id.graph2);
         layout.addView(graphView_lum);
@@ -89,24 +98,58 @@ public class GraphFragment extends Fragment {
 
                     List<Frame> lframes = mCtx.mCameraController.getFrames();
 
-                    GraphView.GraphViewData data1[] = new GraphView.GraphViewData[lframes.size()];
-                    GraphView.GraphViewData data2[] = new GraphView.GraphViewData[lframes.size()];
+                    GraphView.GraphViewData data_delay[] = new GraphView.GraphViewData[lframes.size()];
+                    GraphView.GraphViewData data_delay_f[] = new GraphView.GraphViewData[lframes.size()];
+                    GraphView.GraphViewData data_lum[] = new GraphView.GraphViewData[lframes.size()];
+                    GraphView.GraphViewData data_lum_f[] = new GraphView.GraphViewData[lframes.size()];
+                    float fdata_delay[] = new float[lframes.size()];
+                    float fdata_lum[] = new float[lframes.size()];
+
+                    LinearFilter dataFilter = LinearFilter.get(LinearFilter.Filter.KERNEL_GAUSSIAN_11);
 
                     try {
                         for (int i = 0; i < lframes.size(); i++) {
-                            data1[i] = new GraphView.GraphViewData(i, lframes.get(i).delta);
-                            data2[i] = new GraphView.GraphViewData(i, lframes.get(i).luminance / 1000);
+                            fdata_delay[i] = (float) lframes.get(i).delta;
+                            fdata_lum[i] = (float) lframes.get(i).luminance / 1000;
+                        }
+                        dataFilter.apply(fdata_lum);
+                        dataFilter.apply(fdata_delay);
+
+                        for (int i = 0; i < lframes.size(); i++) {
+                            data_delay[i] = new GraphView.GraphViewData(i, lframes.get(i).delta);
+                            data_lum[i] = new GraphView.GraphViewData(i, lframes.get(i).luminance / 1000);
+                            data_delay_f[i] = new GraphView.GraphViewData(i, fdata_delay[i]);
+                            data_lum_f[i] = new GraphView.GraphViewData(i, fdata_lum[i]);
                         }
                     }
                     catch (IndexOutOfBoundsException e) {}
 
-                    GraphViewSeries series = new GraphViewSeries(data1);
-                    graphView_delay.removeAllSeries();
-                    graphView_delay.addSeries(series);
+                    try {
+                        graphView_delay.removeAllSeries();
 
-                    series = new GraphViewSeries(data2);
-                    graphView_lum.removeAllSeries();
-                    graphView_lum.addSeries(series);
+                        GraphViewSeries series = new GraphViewSeries("delay_raw",
+                                new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(70, 70, 70), 3),
+                                data_delay);
+                        graphView_delay.addSeries(series);
+
+                        series = new GraphViewSeries("delay_f",
+                                new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(20, 200, 0), 3),
+                                data_delay_f);
+                        graphView_delay.addSeries(series);
+
+                        graphView_lum.removeAllSeries();
+
+                        series = new GraphViewSeries("luminance_raw",
+                                new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(70, 70, 70), 3),
+                                data_lum);
+                        graphView_lum.addSeries(series);
+
+                        series = new GraphViewSeries("luminance_f",
+                                new GraphViewSeries.GraphViewSeriesStyle(Color.rgb(200, 50, 00), 3),
+                                data_lum_f);
+                        graphView_lum.addSeries(series);
+                    }
+                    catch (NullPointerException e) {}
 
                     graphView_delay.scrollToEnd();
                     graphView_lum.scrollToEnd();
