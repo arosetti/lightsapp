99a6f407a1c39acc44f2bf148b8265473662fae8
Alessandro Rosetti <alessandro.rosetti@gmail.com>
2014-11-06 21:54:18 +0100

    AboutActivity: updated GraphFragment: show a third graph containing the real luminance derivative.

--- app/src/main/java/com/lightsapp/lightsapp/AboutActivity.java
+++ app/src/main/java/com/lightsapp/lightsapp/AboutActivity.java
@@ -41,7 +41,12 @@ public class AboutActivity extends Activity {
                 "<a href=\"mailto:alessandro.rosetti@gmail.com\">Alessandro Rosetti</a><br/>" +
                 "<a href=\"mailto:lazzalf@yahoo.com\">Daniele Lazzarini</a><br/><br/>" +
                 "<b>System Info:</b><br/> Camera: " + video + "<br/><br/>" +
-                "<b>Tips:</b><br/>"));
+                "<b>Open Source: <br/>Android's examples morse class has been modified to our needs.<br/>graphview:3.1.3</b><br/><br/>" +
+                "<b>Tips:</b><br/>These tips may help you having a better experience.<br/>" +
+                "[1] You can't successfully use this program if the external light is too high or if there are too much interferences.<br/>" +
+                "[2] Use the graph section to check if there is a good variation of luminance while transmitting or receiving.<br/>" +
+                "[3] Try to lower the sensitivity, it will dynamically adjust the thresholds and it may successfully translate the signal to text.<br/>" +
+                "[4] Try to use higher base morse interval time. It depends on the camera that can't deliver a good frame rate or the flash led that can't switch on and off quickly."));
     }
 
     @Override
